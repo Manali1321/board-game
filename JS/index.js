b1c660a2f420876abd9a69f6ccb09e19d0058228
manali1321 <patelmanali216@gmail.com>
2022-12-14 22:28:53 -0500

    css

--- JS/index.js
+++ JS/index.js
@@ -1,21 +1,41 @@
-
-
+var balls = document.querySelectorAll('.ball')
 var board = document.getElementById("screen");
-// console.log(boardWidth);
+var ballCount = 0;
+var gameOver = false
+balls[0].addEventListener('animationend',
+    gameOverEvent);
+balls[1].addEventListener('animationend', gameOverEvent);
+balls[2].addEventListener('animationend', gameOverEvent);
+
+
+function gameOverEvent() {
+    balls[0].style.animationPlayState = 'paused'
+    balls[1].style.animationPlayState = 'paused'
+    balls[2].style.animationPlayState = 'paused'
+    if (!gameOver) {
+        alert('game over')
+    }
+}
 
 function startGame() {
-    document.querySelectorAll(".ball").forEach(ball => {
+    balls.forEach(ball => {
         ball.classList.add('ball-move');
         ball.classList.remove('hide');
         ball.style.left = Math.floor(Math.random() * 500 + 1) + 'px';
-    })
-    // ball[0]
-    // document.getElementsByClassName("ball")
+    });
+}
+function reset(num) {
+    balls[num].classList.remove('hide');
+    balls[num].classList.add('ball-move');
+    balls[num].style.left = Math.floor(Math.random() * 500 + 1) + 'px';
+    console.log(balls[num].style.left);
+    ballCount += 1;
+    console.log(ballCount);
+    document.getElementById('show_count').innerText = ballCount;
+}
+function disappear(num) {
+
+    balls[num].classList.add('hide');
+    balls[num].classList.remove('ball-move');
 }
 
-function disappear() {
-    document.querySelectorAll(".ball").forEach(ball => {
-        ball.classList.add('hide');
-        ball.classList.remove('ball-move');
-    })
-}
